e60403118dbcd3598758f8523584fd11b34569f3
Madhu <madhukm2050@gmail.com>
2025-02-06 21:36:33 +0530

    06-02-2025

---
+++ Transform_String.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    ll i = s.length()-1, j = t.length()-1;
+    ll ind = -1;
+    vector<ll> st;
+    while (i >= 0 && j >= 0)
+    {
+        if(s[i] == t[j]){
+            s[i] = '1';
+            t[j] = '1';
+            if(ind == -1)ind = i+1;
+            i--;
+            j--;
+        }
+        else{
+            if(j != t.length()-1)st.pb(i);
+            i--;
+        }
+    }
+    if(j >= 0){
+        cout << -1 << ln;
+        return;
+    }
+    ll sum = i+1;
+    
+    ll count = 0;
+    //cerr << (i+1) << " "<<ind << ln;
+    //debug(s);
+    
+    for(ll x = i+1; x < ind ; x++){
+        if(s[x] == '1')count++;
+        if(s[x] != '1'){
+            sum += count+1;
+        }
+    }
+    
+    ll sum2 = (s.length()-ind)*(count+1);
+    //cout << count << ln;
+    cout << sum+sum2 << ln;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
