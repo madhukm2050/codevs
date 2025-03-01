eb5c831fe901c9d0fdb491b6f583023c05b27dfe
Madhu <madhukm2050@gmail.com>
2025-03-01 22:34:27 +0530

    01-03-2025

---
+++ B_Make_Target.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<vector<char>> vec(n, vector<char>(n,'.'));
+
+    ll top = 0, left = 0, bottom = n-1, right = n-1, count = 0;
+
+    while(top <= bottom && left <= right){
+        for(ll i = left; i <= right; i++){
+            if(count %2 == 0){
+                
+                vec[top][i] = '#';
+            }
+            else{
+                vec[top][i] = '.';
+            }
+        }
+        top++;
+
+        for(ll i = top; i <= bottom; i++){
+            if(count %2 == 0){
+                vec[i][right] = '#';
+            }
+            else{
+                vec[i][right] = '.';
+            }
+        }
+        right--;
+
+        if(top <= bottom){
+            for(ll i = right; i >= left; i--){
+                if(count %2 == 0){
+                    vec[bottom][i] = '#';
+                }
+                else{
+                    vec[bottom][i] = '.';
+                }
+            }
+            bottom--;
+        }
+
+        if(left <= right){
+            for(ll i = bottom; i >= top; i--){
+                if(count %2 == 0){
+                    vec[i][left] = '#';
+                }
+                else{
+                    vec[i][left] = '.';
+                }
+            }
+            left++;
+        }
+        count++;
+    }
+
+    for(ll i = 0; i < n; i++){
+        for(ll j = 0; j < n; j++){
+            cout << vec[i][j];
+        }
+        cout << ln;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
