2d1b73799c6c3014d922fc6b0151bee2d3d2f082
Madhu <madhukm2050@gmail.com>
2025-03-22 21:45:35 +0530

    22-02-2025

---
+++ A_Serval_and_String_Theory.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    string s1 = "";
+    for(ll i = n-1; i >= 0; i--){
+        s1 += s[i];
+    }
+    if(s < s1){
+        YES;
+        return;
+    }
+    if(k == 0){
+        NO;
+        return;
+    }
+
+    bool flag = false;
+    ll count = 0;
+    for(ll i = 0; i < n; i++){
+        if(s[i] < s1[i]){
+            YES;
+        }
+        ll ind = -1;
+        for(ll j = i; j < n; j++){
+            if(s[j] < s1[i]){
+                YES;
+                return;
+            }
+            if(ind == -1 && s[j] == s1[i]){
+                ind = j;
+            }
+        }
+        if(ind != -1){
+            swap(s[ind], s[i]);
+            count++;
+        }
+        if(count == k){
+            NO;
+            return;
+        }
+    }
+    
+    NO;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ B_Full_House_3.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    map<ll,ll> map;
+    for(ll i = 0; i < 7; i++){
+        ll a;
+        cin >> a;
+        map[a]++;
+    }
+    bool flag1 = false, flag2 = false;
+    vector<ll> vec;
+    for(auto e : map){
+        vec.pb(e.ss);
+    }
+    sort(vec.rbegin(), vec.rend());
+    ll count = 0;
+    for(auto e : vec){
+        if(e > 2 && !flag1){
+            flag1 = true;
+        }
+        else if(flag1 && e > 1){
+            flag2 = true;
+        }
+    }
+
+    if(flag1 && flag2){
+        Yes;
+    }
+    else{
+        No;
+    }
+    //debug(vec);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
