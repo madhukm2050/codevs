85c90251d303d3f2fd87397531afc74b21270ad4
Madhu <madhukm2050@gmail.com>
2025-03-24 22:09:52 +0530

    24-02-2025

---
+++ D_Simple_Permutation.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    ll x = n/3 -1;
+
+    if(x <= 0){
+        for(ll i = 1; i <= n; i++){
+            cout << i << " ";
+        }
+        cout << ln;
+        return;
+    }
+
+
+    for(ll i = x; i < n/2 +1; i++){
+        ll count = 0;
+        for(ll j = 1; j <= i; j++){
+            if((i%j)== 0)count++;
+        }
+        cerr<< x << " "<<(n/2+1) << ln;
+        if(count == 2){
+            ll prime = i;
+            vector<ll> vec;
+            for(ll j = 1; j <= prime; j++){
+                vec.pb(j);
+                if(j != prime)vec.pb(2*prime-j);
+            }
+            for(ll j = 2*prime; j <= n; j++){
+                vec.pb(j);
+            }
+
+            rep(i, vec.size()){
+                cout << vec[i] << " ";
+            }
+            cout << ln;
+            return;
+        }
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
