8213f1d595c94321d9531a52a63c4754428aff2c
Madhu <madhukm2050@gmail.com>
2025-03-30 22:26:08 +0530

    29-03-2025 and 30-03-2025

---
+++ C_Make_it_Forest.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+vector<ll> parent;
+vector<ll> rankVec;
+ll find(ll x){
+    if(x == parent[x])return x;
+    return parent[x] = find(parent[x]);
+}
+void unionSets(ll u, ll v) {
+    u = find(u);
+    v = find(v);
+
+    if (u != v) {
+        if (rankVec[u] < rankVec[v]) {
+            swap(u, v);
+        }
+        parent[v] = u;
+        if (rankVec[u] == rankVec[v]) {
+            rankVec[u]++;
+        }
+    }
+}
+
+void solve() {
+    ll n ,m;
+    cin >> n >> m;
+    parent.resize(n+1);
+    rankVec.resize(n+1,0);
+    rep(i,n+1){
+        parent[i] = i;
+    }
+    
+    ll count = 0;
+    rep(i,m){
+        ll x,y;
+        cin >> x >> y;
+
+        ll u = find(x), v = find(y);
+
+        if(u != v){
+            unionSets(u, v);
+        }
+        else{
+            count++;
+        }
+    }
+    cout << count << ln;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
