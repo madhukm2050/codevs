a0abad72e239cf47806e864d73f8a0705a180fa6
Madhu <madhukm2050@gmail.com>
2025-04-01 22:13:33 +0530

    01-04-2025

---
+++ C_Assembly_via_Minimums.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+// struct cmp{
+//     bool operator()(pair<ll,ll> a , pair<ll,ll> b){
+//         return a.ff < b.ff;
+//     }
+// };
+void solve() {
+    ll n;
+    cin >> n;
+    map<ll,ll> map;
+    ll n1 = (n*(n-1))/2ll;
+    ll m1 = 0;
+    rep(i,n1){
+        ll a;
+        cin >> a;
+        map[a]++;
+        m1 = max(m1, a);
+    }
+
+    priority_queue<pair<ll,ll>> pq;
+
+    for(auto e : map){
+        pq.push({-1*e.ff, e.ss});
+    }
+
+    for(ll i = 0; i < n-1; i++){
+        pair<ll,ll> p = pq.top();
+        pq.pop();
+        //cerr << p.ff << " "<<p.ss << ln;
+
+        if(p.ss > n-i-1){
+            pq.push({p.ff, p.ss-n-1-i});
+        }
+        cout << -1*p.ff << " ";
+    }
+    cout << 1000000000<< ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ D_Three_Activities.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<pair<ll,ll>> a,b,c;
+    rep(i,n){
+        ll val;
+        cin >> val;
+        a.pb({val,i});
+    }
+    rep(i,n){
+        ll val;
+        cin >> val;
+        b.pb({val,i});
+    }
+    rep(i,n){
+        ll val;
+        cin >> val;
+        c.pb({val,i});
+    }
+    sort(a.rbegin(), a.rend());
+    sort(b.rbegin(), b.rend());
+    sort(c.rbegin(), c.rend());
+
+    ll ans = 0;
+    for(ll i = 0; i < 3; i++){
+        for(ll j = 0; j < 3; j++){
+            for(ll k = 0; k < 3; k++){
+                if(a[i].ss != b[j].ss && b[j].ss != c[k].ss && a[i].ss != c[k].ss){
+                    ans = max(ans, a[i].ff+b[j].ff+c[k].ff);
+                }
+            }
+        }
+    }
+    cout << ans << ln;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
