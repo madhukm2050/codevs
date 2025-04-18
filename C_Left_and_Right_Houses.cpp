5f3308b159449c844ae2c68e8683809102effff7
Madhu <madhukm2050@gmail.com>
2025-04-18 22:56:14 +0530

    18-04-2025

---
+++ C_Left_and_Right_Houses.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    string s;
+    cin >> n >> s;
+
+    vector<ll> pre(n);
+
+    pre[0] = s[0] =='1';
+    for(ll i = 1; i < n; i++){
+        pre[i] = pre[i-1] + ((s[i] == '1')?1:0);
+    }
+
+    auto sum = [&](ll l, ll r){
+        if(l == 0)return pre[r];
+        return pre[r]-pre[l-1];
+    };
+    //debug(pre);
+
+    ll ind = -1;
+
+    if(sum(0,n-1) >= (n+1)/2){
+        ind = 0;
+    }
+
+    for(ll i = 0; i < n; i++){
+        ll l1 = sum(0, i);
+        ll l2 = i+1-l1;
+        ll r1 = sum(i+1, n-1);
+        ll r2 = n-i-1-r1;
+
+        if(l2 >= l1 && r1 >= r2){
+            if(abs(n-2*(i+1)) < abs(n - 2*ind)){
+                ind = i+1;
+            }
+        }
+    }
+
+    if(ind == -1){
+        cout << 0 << ln;
+        return;
+    }
+    cout << ind << ln;
+
+
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
