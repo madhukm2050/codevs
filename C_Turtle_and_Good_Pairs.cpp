9439b375e6fa44a32ee7c0950db3fc80a83bcda4
Madhu <madhukm2050@gmail.com>
2025-04-20 22:41:28 +0530

    20-04-2025

---
+++ C_Turtle_and_Good_Pairs.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    ll m1 = 0;
+    char c = 'a';
+    vector<ll> vec(26);
+
+    rep(i,n){
+        vec[s[i]-'a']++;
+        if(vec[s[i]-'a'] > m1){
+            m1 = vec[s[i]-'a'];
+            c = s[i];
+        }    
+    }
+    vec[c-'a'] = 0;
+
+    ll j = 0, l = 0;
+    while(l < m1){
+        if(j >= n)j = 1;
+        s[j] = c;
+        l++;
+        j += 2;
+    }
+
+    for(ll i = 0; i < 26; i++){
+        for(ll k = 0; k < vec[i]; k++){
+            if(j >= n)j = 1;
+            s[j] = (char)(i+'a');
+            j += 2;
+        }
+    }
+    cout << s << ln;
+    //debug(vec);
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
