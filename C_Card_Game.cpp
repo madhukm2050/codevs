27093a68fce158336a38d6034df85e71e21043f3
Madhu <madhukm2050@gmail.com>
2025-04-28 23:39:50 +0530

    28-04-2025

---
+++ C_Card_Game.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    ll count1 = 0, count2 = 0;
+    rep(i,n){
+        if(s[i] == 'A'){
+            count1++;
+        }
+        else{
+            count2++;
+        }
+    }
+
+    string s1 = "Alice", s2 = "Bob";
+    //debug(s);
+
+    if(s[0] == s[n-1]){
+        if(s[0] == 'A'){
+            cout << s1 << ln;
+        }
+        else{
+            cout << s2 << ln;
+        }
+    }
+    else if(s[0] == 'A' && count2 == 1){
+        cout << s1 << ln;
+    }
+    else if(s[0] == 'B' && count1 == 1){
+        cout << s2 << ln;
+    }
+    else if(s[0] == 'A' && count2 > 1){
+        cout << s2 << ln;
+    }
+    else if(s[0] == 'B' && s[n-1] == s[n-2]){
+        cout << s1 << ln;
+    }
+    else{
+        cout << s2 << ln;
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
