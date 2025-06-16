808d1178d8f0449403b123524365b751cb5f68f8
Madhu <madhukm2050@gmail.com>
2025-06-16 22:12:03 +0530

    16-06-2025

---
+++ B_Good_Start.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll w, h, a, b;
+    cin >> w >> h >> a >> b;
+
+    ll x1, y1 ,x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    cerr << x1 << " "<< y1 << " "<< x2 << " "<<y2 << ln;
+
+    ll p1 = x1+a-1, p2 = x2;
+    ll p3 = y1+b-1, p4 = y2;
+
+    cerr << p1 << " "<< p2 << " "<< p3 << " "<< p4 << ln;
+
+    if(p1 >= p2){
+        if((abs(p3-p4))%w == 0){
+            Yes;
+        }
+        else{
+            No;
+        }
+    }
+    else if(p3 >= p4){
+        if((abs(p2-p1))%h == 0){
+            Yes;
+        }
+        else{
+            No;
+        }
+    }
+    else{
+        Yes;
+    }
+
+    cerr << p1 << " "<< p2 << ln;
+    cerr << p3 << " "<< p4 << ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
