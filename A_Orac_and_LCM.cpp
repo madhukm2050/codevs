2e6db7f909f86de7c64351d0d4cc97bc61384337
Madhu <madhukm2050@gmail.com>
2025-06-27 23:16:04 +0530

    27-06-2025

---
+++ A_Orac_and_LCM.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+ll expo(ll b, ll p){
+    ll ans = 1;
+    while(p!= 0){
+        if((p&1) != 0){
+            ans *= b;
+        }
+        b *= b;
+        p = p >> 1;
+    }
+    return ans;
+}
+
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> vec(n);
+    rep(i,n)cin >> vec[i];
+
+    ll N = 200001;
+
+    vector<vector<ll>> primes(N);
+
+    for(ll i = 0; i < n; i++){
+        ll x = vec[i];
+        for(ll j = 2; j*j <= x; j++){
+            if(x%j == 0){
+                ll count = 0;
+                while(x%j == 0){
+                    x = x/j;
+                    count++;
+                }
+                primes[j].pb(count);
+            }
+        }
+        if(x > 1)primes[x].pb(1);
+    }
+
+    ll ans = 1;
+    for(ll i = 2; i < N; i++){
+        sort(all(primes[i]));
+        if(primes[i].size() < n-1)continue;
+        if(primes[i].size() == n){
+            ans *= expo(i,primes[i][1]);
+        }
+        else{
+            ans *= expo(i,primes[i][0]);
+        }
+    }
+    
+    
+    cout << ans << ln;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
