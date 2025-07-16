3f82eb0dd8232af250dde750b4e5486815264ebf
Madhu <madhukm2050@gmail.com>
2025-07-16 23:35:58 +0530

    16-07-2025

---
+++ String_Deletions.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<bool> pre1(n),pre0(n),suff1(n),suff0(n);
+    bool pre11 = false, pre00 = false, suff11 = false, suf00 = false;
+
+    for(ll i = 0; i < n; i++){
+        if(s[i] == '1')pre11 = true;
+        if(s[i] == '0')pre00 = true;
+        if(s[n-1-i] == '0')suf00 = true;
+        if(s[n-1-i] == '1')suff11 = true;
+        
+        pre0[i] = pre00;
+        pre1[i] = pre11;
+        suff1[n-1-i] = suff11;
+        suff0[n-1-i] = suf00;
+    }
+    // debug(pre0);
+    // debug(pre1);
+    // debug(suff0);
+    // debug(suff1);
+    ll count = 0;
+    bool flag = false;
+    rep(i,n){
+        if(s[i] == '1'){
+            if(pre0[i] && suff0[i]){
+                count++;
+                flag = true;
+            }
+        }
+        else{
+            if(pre1[i] && suff1[i]){
+                count++;
+                flag =true;
+            }
+        }
+    }
+    ll ans = n-count;
+    if(flag && s[0] != s[n-1])ans++;
+
+    cout << ans<< ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
