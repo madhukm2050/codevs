7d4e3ae3e81f21d28c6aeeb05e0b3dd2ce4d0d29
Madhu <madhukm2050@gmail.com>
2025-07-24 23:14:25 +0530

    24-07-2025

---
+++ Grid_Paths_I.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+ll dfs(ll i, ll j,ll n,vector<string> &vec, vector<vector<ll>> &dp){
+    //cerr << i << " "<< j << ln;
+    if(i == n-1 && j == n-1)return 1;
+    if(dp[i][j] != -1)return dp[i][j];
+    ll count = 0;
+    if(i+1 < n && vec[i+1][j] == '.')count = (count%MOD + dfs(i+1, j, n, vec, dp)%MOD)%MOD;
+    if(j+1 < n && vec[i][j+1] == '.')count = (count%MOD + dfs(i, j+1, n, vec, dp)%MOD)%MOD;
+    return dp[i][j] = count;
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vector<string> vec(n);
+    rep(i,n)cin >> vec[i];
+
+    vector<vector<ll>> dp(n, vector<ll>(n, 0));
+    if(vec[0][0] == '.')dp[0][0] = 1;
+
+    for(ll i = 0; i < n; i++){
+        for(ll j = 0; j < n; j++){
+            if(vec[i][j] == '.'){
+                if(i > 0)dp[i][j] = (dp[i][j]%MOD + dp[i-1][j]%MOD)%MOD;
+                if(j > 0)dp[i][j] = (dp[i][j]%MOD + dp[i][j-1]%MOD)%MOD;
+            }
+        }
+    }
+    cout << dp[n-1][n-1] << ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
