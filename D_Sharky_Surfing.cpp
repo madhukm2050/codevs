a63e5867cc1cceb279555453e6f1c3a155e7599c
madhu <madhukm2050@gmail.com>
2025-08-24 22:18:21 +0530

    24-08-2025

---
+++ D_Sharky_Surfing.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+    ll n, m, l;
+    cin >> n >> m >> l;
+
+    //debug(n);
+    //debug(m);
+    //debug(l);
+
+    vector<pair<ll,ll>> hurdle(n);
+    rep(i,n){
+        cin >> hurdle[i].ff >> hurdle[i].ss;
+    }
+    //debug(hurdle);
+
+    vector<pair<ll,ll>> powerup(m);
+    rep(i,m){
+        cin >> powerup[i].ff >> powerup[i].ss;
+    }
+
+    //debug(hurdle);
+    //debug(powerup);
+
+    ll power = 1;
+    priority_queue<ll> pq;
+
+    ll j = 0, count = 0;
+    rep(i,n){
+        while(j < m && powerup[j].ff < hurdle[i].ff){
+            pq.push(powerup[j].ss);
+            j++;
+        }
+        //cout << sz(pq) << ln;
+
+        ll d = hurdle[i].ss-hurdle[i].ff+2;
+        while(!pq.empty() && d > power){
+            //cerr << power << " "<< d << ln;
+            power += pq.top();
+            pq.pop();
+            count++;
+        }
+        //cerr << power << ln;
+        if(d > power){
+            cout << -1 << ln;
+            return;
+        }
+        //cerr << power << ln;
+    }
+    cout << count << ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
