41c1eeb6a43d002bfe41b09538301b8b92204742
madhu <madhukm2050@gmail.com>
2025-10-17 23:49:29 +0530

    17-10-2025

---
+++ D_Beautiful_Permutation.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+vector<ll> vec1 = {0,5, 6 , 9, 11, 15, 21, 28};
+vector<ll> vec2 = {0,5, 6 , 10, 13, 17, 23, 30};
+
+ll ask(ll q, ll l, ll r){
+    cout << q << " "<< l << " "<< r << endl;
+
+    // if(q == 1){
+    //     return vec1[r]-vec1[l-1];
+    // }
+    // else{
+    //     return vec2[r]-vec2[l-1];
+    // }
+    ll a;
+    cin >> a;
+    return a;
+}
+void solve() {
+    ll n;
+    cin >> n;
+    ll l1 = 1, r1 = n, ind1 = 0;
+
+    while(l1 < r1){
+        ll mid = (l1+r1)/2;
+        ll v1 = ask(1, l1, mid), v2 = ask(2, l1, mid);
+
+        if(v1 == v2){
+            l1 = mid+1;
+        }
+        else{
+            ind1 = mid;
+            r1 = mid;
+        }
+    }
+    ll l2 = 1, r2 = n, ind2 = 0;
+
+    while(l2 < r2){
+        ll mid = (l2+r2)/2;
+        ll v1 = ask(1, mid, r2), v2 = ask(2, mid, r2);
+
+        if(v1 == v2){
+            r2 = mid-1;
+        }
+        else{
+            ind2 = mid;
+            l2 = mid;
+        }
+    }
+    cout << "! "<< ind1 << " "<< ind2 << endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
