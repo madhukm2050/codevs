7659a4a5d3a7481385708a94c1af78c4d779763d
madhu <madhukm2050@gmail.com>
2026-01-07 22:11:13 +0530

    07-01-2026

---
+++ B_Berland_Crossword.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+bool helper(ll n, ll u, ll r, ll d, ll l, ll val){
+    for(ll i = 0; i < 4; i++){
+        if(i == 0 && (val&(1 << i)) != 0){
+            u--;
+            l--;
+        }
+        if(i == 1 && (val&(1 << i)) != 0){
+            u--;
+            r--;
+        }
+        if(i == 2 && (val&(1 << i)) != 0){
+            d--;
+            l--;
+        }
+        if(i == 3 && (val & (1 << i)) != 0){
+            r--;
+            d--;
+        }
+    }
+    //cerr << val << " "<< u << " "<< r << " "<< d << " "<< l << ln;
+    if(u >= 0 && u <= n-2 && r >= 0 && r <= n-2 && l >= 0 && l <= n-2 && d >= 0 && d <= n-2){
+        //cerr << val << " "<< u << " "<< r << " "<< d << " "<< l << ln;
+        return true;
+    }
+    return false;
+}
+
+void solve() {
+    ll n, u, r, d, l;
+
+    cin >> n >> u >> r >> d >> l;
+
+    for(ll i = 0; i <= 16; i++){
+        if(helper(n, u, r, d, l, i)){
+            //cerr << i << ln;
+            YES;
+            return;
+        }
+    }
+    NO;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
