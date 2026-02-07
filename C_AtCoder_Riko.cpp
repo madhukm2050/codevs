a5abf419ca8e12c43bb4ddfeb04e043987d34e10
madhu <madhukm2050@gmail.com>
2026-02-07 23:27:10 +0530

    07-02-2026

---
+++ C_AtCoder_Riko.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define No cout<<"No"<<endl;
+#define rep(i,n) for(int i = 0; i < n; i++)
+
+#ifdef madhukm2050
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+template <class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset<T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map<T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+bool helper(vector<ll> &vec, ll sum){
+    ll i = 0, j = sz(vec)-1;
+
+    while(i <= j){
+        if(vec[i] == sum){
+            i++;
+        }
+        else if(i == j){
+            return false;
+        }
+        else if(i < j && vec[j] == sum){
+            j--;
+        }
+        else if(vec[i]+vec[j] == sum){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vector<ll> vec(n);
+    rep(i,n)cin >> vec[i];
+    sort(all(vec));
+
+    ll sum1 = 0, max1 = 0;
+    rep(i,n){
+        sum1 += vec[i];
+        max1 = max(max1, vec[i]);
+    }
+    set<ll> st;
+    for(ll i = 1; i *i <= sum1; i++){
+        if((sum1%i) == 0){
+            ll s1 = i;
+            ll s2 = sum1/i;
+
+
+            if(helper(vec, s1))st.insert(s1);
+            if(s2 >= max1 && helper(vec, s2))st.insert(s2);
+        }
+    }
+
+    for(auto e : st){
+        cout << e << " ";
+    }
+    cout << ln;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
